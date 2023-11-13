6b409d0e1383842ada228b536d084195aa533b97
qiuwenxing <qiuwenxing@delilegal.com>
2023-11-13 14:17:15 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
-import Inspect from 'vite-plugin-inspect'
-import vitePluginOss from './src/index'
+import Inspect from "vite-plugin-inspect";
+import ViteOSSPluginPro from "./src/index";
+
+const prod = process.env.NODE_ENV === "production";
+
 export default {
   plugins: [
     Inspect(),
-    vitePluginOss({
-      from: './dist/**', // 上传那个文件或文件夹  可以是字符串或数组
-      // dist: "/test",  // 需要上传到oss上的给定文件目录
-      region: 'xxxx',
-      accessKeyId: 'xxxx',
-      accessKeySecret: 'xxxxxxx',
-      bucket: 'xxxxx',
-      test: true
-    }),
-  ]
-}
+    prod &&
+      ViteOSSPluginPro({
+        from: "./dist/assets/**", // 上传那个文件或文件夹  可以是字符串或数组
+        dist: "/static", // 需要上传到oss上的给定文件目录
+        region: "xxxx",
+        accessKeyId: "xxxx",
+        accessKeySecret: "xxxxxxx",
+        bucket: "xxxxx",
+        test: true,
+        cdnUrl: "https://cdn.xxx.com",
+      }),
+  ],
+};
